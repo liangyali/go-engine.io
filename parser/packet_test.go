c84536edf467970916a990b7b9b41f1b9c5e2ad9
liangyali <me@liangyali.com>
2016-12-16 15:05:16 +0800

    add packet

---
+++ parser/packet_test.go
@@ -0,0 +1,10 @@
+package parser
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPacketType(t *testing.T) {
+	assert.Equal(t, 1, 1, "Hello")
+}
---
+++ server.go
@@ -0,0 +1,3 @@
+package engineio
+
+import ()
